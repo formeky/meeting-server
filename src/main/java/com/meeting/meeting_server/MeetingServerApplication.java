5e9b999f683dba6bd51f9be1e6dfab33468a968c
zcw <2352427469@qq.com>
2021-09-03 22:20:39 +0800

    refactor: 项目初始化

---
+++ src/main/java/com/meeting/meeting_server/MeetingServerApplication.java
@@ -0,0 +1,13 @@
+package com.meeting.meeting_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingServerApplication.class, args);
+    }
+
+}
